8036cc3a8b098d44ed2bb968d521e9771baae2a7
subiksha <subiksharameshkanna@gmail.com>
2026-03-26 14:57:17 +0530

    Force proxy middleware to Node runtime

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server'
 import * as jose from 'jose'
 
+export const runtime = 'nodejs'
+
 const PROTECTED: Record<string, string> = {
   '/admin': 'ADMIN',
   '/member': 'MEMBER',
